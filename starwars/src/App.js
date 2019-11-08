ad0b717e2d2ec2d92b027b26ffb0322d2820aa5e
newbi462 <isthisemailfree@gmail.com>
2019-11-08 11:46:46 -0500

    Follow the documentation to learn how to fetch a list of "people". However, don't spend too long on this. Here is a link for you to follow if you've looked around the docs for about 15 minutes or so and haven't found where to go - Secret Link to Awesomeness 🤫.  Set the data you fetch to state. | done

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 import './App.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [apiData, setApiData] = useState([{}])
+  const [callType, setCallType] = useState("people")
+  const [number, setNumber] = useState(1)
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios.get(`https://swapi.co/api/${callType}/?page=${number}`)
+    .then(response => {
+      console.log(response.data.results)
+      setApiData(response.data.results)
+    })
+    .catch( error => {
+      console.log("sorry no dogs", error)
+    })
+  },[callType, number])
 
   return (
     <div className="App">
